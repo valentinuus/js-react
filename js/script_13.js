31422ee97c3afedc1ddca31aa7c3ed21c98a3313
valchik <valentinus@list.ru>
2024-10-30 15:04:57 +0300

    Lesson 13

---
+++ js/script_13.js
@@ -0,0 +1,19 @@
+const numberOfFilms = prompt("Сколько фильмов вы уже посмотреле", "1");
+
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false,
+};
+
+const a = prompt("какой последний фильм", ""),
+		b = prompt("баллов поставите", "");
+		c = prompt("какой последний фильм", ""),
+		d = prompt("баллов поставите", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB)
