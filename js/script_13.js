2cd7ff79f86c8215881804c185f1a55e0fcd1dcb
valchik <valentinus@list.ru>
2024-10-30 17:06:17 +0300

    Lesson 13

---
+++ js/script_13.js
@@ -0,0 +1,42 @@
+"use strict"
+
+
+// IF ELSE
+// if (4 == 9) {
+// 	console.log('ОК!!');
+// } else {
+// 	console.log('error');
+// }
+
+
+
+// IF, ELSE IF, ELSE
+const num = 100;
+
+// if (num < 50) {
+// 	console.log('error');
+// } else if (num > 100) {
+// 	console.log('очень много')
+// } else {
+// 	console.log('оки')
+// }
+
+// ТЕРНАРНЫЙ ОПЕРАТОР
+
+// (num == 50) ? console.log('OK') : console.log('error')
+
+
+// SWITCH
+switch (num) {
+	case 49:
+		console.log('не верно');
+		break;
+	case 100:
+		console.log('не верно');
+		break;
+	case 50:
+		console.log('ок');
+		break;
+	default:
+		console.log('не в этот раз')
+}
