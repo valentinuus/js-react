164b888606927e967ba9a83facb68ae5ac12a8a8
valchik <valentinus@list.ru>
2024-11-01 06:05:10 +0300

    Lesson 15

---
+++ js/script_14.js
@@ -0,0 +1,38 @@
+"use strict"
+//ЦИКЛЫ
+let num = 50;
+
+
+// // этот цикл выполняется пока выполняется условие
+// // while (num <= 55) {
+// // 	console.log(num);
+// // 	num++;
+// // }
+
+
+// // можем сначала чтото сделать , а затем проверить условие
+// // do {
+// // 	console.log(num);
+// // 	num++;
+// // }
+// // while (num < 55);
+
+// //Цикл for 
+
+// for (let i = 1; i < 8; i++) {
+// 	console.log(num);
+// 	num++;
+// }
+
+// //Цикл for c break
+
+
+for (let i = 1; i < 10; i++) {
+	if (i === 2) {
+		// break;
+		continue;
+	}
+
+	console.log(i);
+
+}
---
+++ js/script_15.js
@@ -0,0 +1,44 @@
+"use strict"
+
+
+const numberOfFilms = prompt("Сколько фильмов вы уже посмотреле");
+
+
+
+
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false,
+};
+
+// for (let i = 0; i < 1; i++) {
+// 	const a = prompt("какой последний фильм", ""),
+// 		b = prompt("баллов поставите", "");
+// 	if (a != null && b != null && a != '' && b != '' && a.length < 10) {
+// 		personalMovieDB.movies[a] = b;
+// 		console.log('done');
+// 	} else {
+// 		console.log('error');
+// 		i--;
+// 	}
+// }
+
+
+
+if (personalMovieDB.count < 10) {
+	console.log('мало');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+	console.log('norm')
+} else if (personalMovieDB.count >= 30 ) {
+	console.log('много')
+} else {
+	console.log('эошибка')
+}
+
+
+
+
+console.log(personalMovieDB)
